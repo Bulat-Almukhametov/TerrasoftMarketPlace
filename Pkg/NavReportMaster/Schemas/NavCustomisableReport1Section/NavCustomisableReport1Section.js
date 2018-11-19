94a524ac6d2b220dd31e0dea3a062867998d9d12
MisterArslan <arsburs@gmail.com>
2018-11-19 14:05:18 +0300

    Исправлен баг с выбрасыванием из карточки при сохранении настроек колонки нового отчета. Переопределен метод addRecord в NavCustomisableReport1Section, добавлена логика HistoryState. Переопределен метод _closePage в NavCustomisableReport1Page.

--- Pkg/NavReportMaster/Schemas/NavCustomisableReport1Section/NavCustomisableReport1Section.js
+++ Pkg/NavReportMaster/Schemas/NavCustomisableReport1Section/NavCustomisableReport1Section.js
@@ -1,8 +1,43 @@
-define("NavCustomisableReport1Section", [], function() {
-	return {
-		entitySchemaName: "NavCustomisableReport",
-		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
-		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
-		methods: {}
-	};
+define("NavCustomisableReport1Section", ['ConfigurationEnums'], function (ConfigurationEnums) {
+    return {
+        entitySchemaName: "NavCustomisableReport",
+        details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+        diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+        methods: {
+            /** Переопределение метода добавления записи  */
+            addRecord: function (typeColumnValue) {
+                var guid = Terrasoft.generateGUID();
+                this.sandbox.publish("PushHistoryState", {
+                    hash: "CardModuleV2/NavCustomisableReport1Page/edit/" + guid
+                });
+                this.showBodyMask();
+                var stateObj = {
+                    isSeparateMode: true,
+                    schemaName: "NavCustomisableReport1Page",
+                    entitySchemaName: "NavCustomisableReport",
+                    operation: ConfigurationEnums.CardStateV2.ADD,
+                    primaryColumnValue: guid,
+                    valuePairs: [
+                        {
+                            name: "Id",
+                            value: guid
+                        }
+                    ],
+                    isInChain: true
+                };
+                this.sandbox.publish("PushHistoryState", {
+                    hash: "CardModuleV2/NavCustomisableReport1Page/edit/" + guid,
+                    silent: true,
+                    stateObj: stateObj
+                });
+                var moduleName = "CardModuleV2";
+                var moduleParams = {
+                    renderTo: this.renderTo,
+                    id: this.getChainCardModuleSandboxId(typeColumnValue),
+                    keepAlive: false
+                };
+                this.sandbox.loadModule(moduleName, moduleParams);
+            }
+        }
+    };
 });
