21906dd2eb343d3ff1a6a1badba96cfb15895949
MisterArslan <arsburs@gmail.com>
2019-02-12 16:50:54 +0300

    Рефакторинг алгоритма проверки лицензии

--- External Solutions/ExtLib.NavAd/ExtLib/AdIntegration/AdElementsHelper.cs
+++ External Solutions/ExtLib.NavAd/ExtLib/AdIntegration/AdElementsHelper.cs
@@ -10,6 +10,7 @@ namespace AdIntegration.AD
         protected string _DistinguishedName;
         protected string _RequestText;
         protected UserConnection _UserConnection;
+
         public AdElementsHelper(UserConnection userConnection, AdCredentials credentials, string distinguishedName, string requestText)
         {
             _Credentials = credentials;
@@ -17,15 +18,9 @@ namespace AdIntegration.AD
             _RequestText = requestText;
             _UserConnection = userConnection;
         }
+
         public AdElement[] GetAllElements()
         {
-            // Узнаем наличие лицензии
-            var licHelper = new LicHelper(_UserConnection);
-            bool hasLicense = licHelper.GetHasOperationLicense("NavAd.Use");
-            // Узнаем находится ли приложение в демо режиме
-            var _appConnection = new AppConnection();
-            var isDemoModeObj = _appConnection.SystemUserConnection.ApplicationData["IsDemoMode"];
-            bool isDemoMode = (isDemoModeObj == null) ? true : Convert.ToBoolean(isDemoModeObj);
             // Получаем записи из Active Directory
             AdElement[] entries;
             using (var ldp = new LdapSync(_Credentials.ServerName, _Credentials.Login, _Credentials.Password, _Credentials.AuthentificationType))
@@ -33,12 +28,25 @@ namespace AdIntegration.AD
                 entries = ldp.GetEntriesWithAttributes(_DistinguishedName, _RequestText);
             }
             // Если демо режим илт нет лицензии, берем только 50 записей
-            if (isDemoMode || !hasLicense)
+            if (IsDemoMode() || !HasLicense("NavAd.Use"))
             {
                 entries = entries?.Take(50).ToArray();
             }
             // Возвращаем записи
             return entries;
+        }
+
+        private bool HasLicense(string sysPackageOperationCode)
+        {
+            var licHelper = new LicHelper(_UserConnection);
+            return licHelper.GetHasOperationLicense(sysPackageOperationCode);
+        }
+
+        private bool IsDemoMode()
+        {
+            var appConnection = new AppConnection();
+            var isDemoModeObj = appConnection.SystemUserConnection.ApplicationData["IsDemoMode"];
+            return (isDemoModeObj == null) ? true : Convert.ToBoolean(isDemoModeObj);
         }
     }
 }
